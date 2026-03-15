0f963bba449cbade8841048342499aaf652de66b
salah Alhaji <118366705+salahxiv@users.noreply.github.com>
2026-03-15 03:27:14 +0000

    fix: Correct Splid App ID (991473495) + add more competitors

--- src/config.js
+++ src/config.js
@@ -3,13 +3,24 @@ module.exports = {
     amber_competitors: [
       { name: "Time Out", iosId: "402592703", platform: "ios" },
       { name: "Rest - Break Reminders", iosId: "1141235580", platform: "ios" },
-      { name: "Eye Care 20 20 20", iosId: "1107610128", platform: "ios" }
+      { name: "Eye Care 20 20 20", iosId: "1107610128", platform: "ios" },
+      { name: "Stretchly", iosId: "1494402311", platform: "ios" },
+      { name: "Awareness", iosId: "409585261", platform: "ios" },
+      { name: "Be Focused Pro", iosId: "961632517", platform: "ios" },
+      { name: "Lungo", iosId: "1276234754", platform: "ios" },
+      { name: "Hand Mirror", iosId: "1502839586", platform: "ios" },
+      { name: "Focus Flow", iosId: "1423210932", platform: "ios" },
+      { name: "Breaks For Eyes", iosId: "571309928", androidId: "net.thewinfregroup.breakforeyes", platform: "both" }
     ],
     split_competitors: [
       { name: "Splitwise", iosId: "458023433", androidId: "com.Splitwise.SplitwiseMobile", platform: "both" },
       { name: "Tricount", iosId: "613510802", androidId: "com.tribab.tricount", platform: "both" },
       { name: "Settle Up", iosId: "989887803", androidId: "cz.destil.settleup", platform: "both" },
-      { name: "Spliit", iosId: "6450860552", platform: "ios" }
+      { name: "Spliit", iosId: "6450860552", platform: "ios" },
+      { name: "Splid", iosId: "991473495", androidId: "de.timo_reymann.splid", platform: "both" },
+      { name: "IOU", iosId: "493846098", platform: "ios" },
+      { name: "Kittysplit", iosId: "1060656606", platform: "ios" },
+      { name: "Tab", iosId: "1440200499", androidId: "com.tabapp.tab", platform: "both" }
     ]
   },
   categories: {
