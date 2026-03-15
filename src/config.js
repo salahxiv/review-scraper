fb6878533314e1bba3944bd6ad2ca4bec5268039
salah Alhaji <118366705+salahxiv@users.noreply.github.com>
2026-03-15 00:04:39 +0000

    feat: add src/config.js

---
+++ src/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: {
+    amber_competitors: [
+      { name: "Time Out", iosId: "402592703", platform: "ios" },
+      { name: "Rest - Break Reminders", iosId: "1141235580", platform: "ios" },
+      { name: "Eye Care 20 20 20", iosId: "1107610128", platform: "ios" }
+    ],
+    split_competitors: [
+      { name: "Splitwise", iosId: "458023433", androidId: "com.Splitwise.SplitwiseMobile", platform: "both" },
+      { name: "Tricount", iosId: "613510802", androidId: "com.tribab.tricount", platform: "both" },
+      { name: "Settle Up", iosId: "989887803", androidId: "cz.destil.settleup", platform: "both" },
+      { name: "Spliit", iosId: "6450860552", platform: "ios" }
+    ]
+  },
+  categories: {
+    feature_request: ["wish", "would be nice", "please add", "missing", "need", "want", "should have", "add a", "feature"],
+    ux_complaint: ["confusing", "hard to", "difficult", "unintuitive", "unclear", "complicated"],
+    bug: ["crash", "bug", "broken", "not working", "error", "freeze"],
+    pricing: ["expensive", "too much", "paywall", "subscription", "price", "free", "pay"]
+  }
+};
